6e9e6ac1a53eed15fa7fb36362a38ee79f236b2e
Arkadiusz Matyska <arkadiusz.matyska@gmail.com>
2023-02-06 18:14:10 +0100

    add main declarations

---
+++ main.js
@@ -0,0 +1,11 @@
+const currentNumber = document.querySelector(".currentNumber");
+const previousNumber = document.querySelector(".previousNumber");
+const mathSign = document.querySelector(".mathSign");
+const numbersButtons = document.querySelectorAll(".number");
+const operatorsButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+const calcHistory = document.querySelector(".history");
+const btnHistory = document.querySelector(".history-btn");
+
+let result = "";
